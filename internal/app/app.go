da1dcf76c99a9c4954be32d76efaf200caad5af0
Rustam Yusupov <i@rstm.me>
2024-08-15 11:19:55 +0200

    Render wishes

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,6 +6,45 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+type Wish struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Link     string `json:"link"`
+	Price    string `json:"price"`
+	Currency string `json:"currency"`
+}
+
+var Wishes = []Wish{
+	{
+		ID:       "1",
+		Name:     "Apple Macbook Pro 13",
+		Link:     "https://www.apple.com/macbook-pro-13/",
+		Price:    "2000",
+		Currency: "$",
+	},
+	{
+		ID:       "2",
+		Name:     "Apple iPhone 12",
+		Link:     "https://www.apple.com/iphone-12/",
+		Price:    "1000",
+		Currency: "$",
+	},
+	{
+		ID:       "3",
+		Name:     "Apple Watch Series 6",
+		Link:     "https://www.apple.com/apple-watch-series-6/",
+		Price:    "500",
+		Currency: "$",
+	},
+	{
+		ID:       "4",
+		Name:     "Jack and Jones T-shirt with a cat",
+		Link:     "https://www.jackjones.com/",
+		Price:    "50",
+		Currency: "$",
+	},
+}
+
 func Run() {
 	engine := html.New("./web", ".html")
 	app := fiber.New(fiber.Config{
@@ -14,7 +53,8 @@ func Run() {
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("index", fiber.Map{
-			"title": "Wishlist",
+			"Title":  "Wishlist",
+			"Wishes": Wishes,
 		})
 	})
 
