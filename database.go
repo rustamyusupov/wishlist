9a857055228aa80cf63bd666ad19108eceb37d68
Rustam Yusupov <i@rstm.me>
2024-08-20 10:43:15 +0200

    Initialize database and create wishes table

---
+++ database.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var db *sql.DB
+
+func Connect() *sql.DB {
+	if db != nil {
+		return db
+	}
+
+	db, err := sql.Open("sqlite3", "wishes.sqlite")
+	if err != nil {
+		log.Fatalf("🔥 failed to connect to the database: %s", err.Error())
+	}
+
+	return db
+}
+
+func InitDB() error {
+	db := Connect()
+
+	stmt := `CREATE TABLE IF NOT EXISTS wishes (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		created_at TIMESTAMP DEFAULT DATETIME
+	  );`
+
+	_, err := db.Exec(stmt)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
