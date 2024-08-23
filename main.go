38c4271890bb90200590106dc3b7e87470685a69
Rustam Yusupov <i@rstm.me>
2024-08-23 17:10:46 +0200

    Add route comment for creating and editing wishes

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ func main() {
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
 	http.HandleFunc("/", controllers.GetIndex)
+	// TODO: plus button, add page, create wish
+	// TODO: edit button, edit page, update wish
 
 	log.Println("🚀 Starting up on port 3000")
 	log.Fatal(http.ListenAndServe(":3000", nil))
