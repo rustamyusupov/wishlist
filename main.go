8f78584f1e3e74d897434a37754cb414e0b7fcc1
Rustam Yusupov <i@rstm.me>
2025-01-15 19:01:03 +0200

    Add method override middleware to support PUT, PATCH, and DELETE requests

--- main.go
+++ main.go
@@ -10,6 +10,23 @@ import (
 
 const port = 3000
 
+func MethodOverride(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			switch method := r.PostFormValue("_method"); method {
+			case http.MethodPut:
+				fallthrough
+			case http.MethodPatch:
+				fallthrough
+			case http.MethodDelete:
+				r.Method = method
+			default:
+			}
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("./assets"))
 	http.Handle("GET /assets/", http.StripPrefix("/assets/", fs))
@@ -23,7 +40,7 @@ func main() {
 	http.HandleFunc("DELETE /api/wishes/{id}", controllers.Delete)
 
 	log.Printf("🚀 Starting up on http://localhost:%d\n", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), MethodOverride(http.DefaultServeMux))
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
