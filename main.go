8275e36f55ffb418b870414c195e5d26cceeb20d
Rustam Yusupov <i@rstm.me>
2025-01-16 17:35:11 +0200

    Remove method override middleware and update route to use PATCH for wishes

--- main.go
+++ main.go
@@ -10,23 +10,6 @@ import (
 
 const port = 3000
 
-func MethodOverride(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost {
-			switch method := r.PostFormValue("_method"); method {
-			case http.MethodPut:
-				fallthrough
-			case http.MethodPatch:
-				fallthrough
-			case http.MethodDelete:
-				r.Method = method
-			default:
-			}
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	fs := http.FileServer(http.Dir("./assets"))
 	http.Handle("GET /assets/", http.StripPrefix("/assets/", fs))
@@ -36,11 +19,11 @@ func main() {
 	http.HandleFunc("GET /edit/{id}", controllers.Edit)
 
 	http.HandleFunc("POST /api/wishes", controllers.Post)
-	http.HandleFunc("PUT /api/wishes/{id}", controllers.Put)
+	http.HandleFunc("PATCH /api/wishes/{id}", controllers.Patch)
 	http.HandleFunc("DELETE /api/wishes/{id}", controllers.Delete)
 
 	log.Printf("🚀 Starting up on http://localhost:%d\n", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), MethodOverride(http.DefaultServeMux))
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
